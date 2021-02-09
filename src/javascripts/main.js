ae152e9749561751ed572a792203cfdac5033be7
Taichi-Murakami <64308722+taichimurakami-github@users.noreply.github.com>
2021-02-09 21:49:05 +0900

    typescript!!!!

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,6 +2,9 @@ import './reactApp.jsx';
 import my from './my.js';
 import '../stylesheets/main.scss';
 
+import add from './add.ts';
+
 
 console.log("webpacktest");
 my();
+console.log(add(3, 10));
